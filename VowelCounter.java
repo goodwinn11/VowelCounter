b5c7d7f4a574002d15f7af1c69770d2562616de7
brantschuleichner <132306028+brantschuleichner@users.noreply.github.com>
2023-05-01 16:14:21 -0500

    Create VowelCounter.java

---
+++ VowelCounter.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class VowelCounter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a string:");
+        String input = scanner.nextLine();
+
+        int vowelCount = countVowels(input);
+        System.out.println("Number of vowels in the string: " + vowelCount);
+    }
+
+    public static int countVowels(String str) {
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            char ch = Character.toLowerCase(str.charAt(i));
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                count++;
+            }
+        }
+        return count;
+    }
+}
